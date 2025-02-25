9efcacb9547dd4d6e9fbfd8eb10bb636f7f059e5
MukhutdinovRR <mukhutdinov.ruslan@gmail.com>
2025-02-25 10:43:44 +0500

    mvp

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,74 @@
 <script setup lang="ts">
-import Picker from './components/Picker.vue';
+import Picker from "./components/Picker.vue";
 </script>
 
 <template>
   <div class="embla">
-    <div class="embla__ios-picker">
-      <div class="embla__ios-picker__scene">
-        <div
-          class="embla__ios-picker__viewport embla__ios-picker__viewport--perspective-right"
-        >
-          <Picker />
-        </div>
+    <div class="embla-picker">
+      <div class="embla-picker__scene">
+        <Picker />
       </div>
-      <div class="embla__ios-picker__label">min</div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.embla {
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 22.2rem;
+  max-width: 30rem;
+  margin-left: auto;
+  margin-right: auto;
+  border: 1px solid var(--text-high-contrast);
+  overflow: hidden;
+  border-radius: 16px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.embla:before,
+.embla:after {
+  position: absolute;
+  left: 0;
+  right: 0;
+  content: "";
+  display: block;
+  height: calc(50% - 32px / 2);
+  z-index: 1;
+  pointer-events: none;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.embla:before {
+  top: -0.5px;
+  border-bottom: 0.5px solid rgba(var(--text-high-contrast-rgb-value), 0.3);
+  background: linear-gradient(
+    to top,
+    rgba(var(--background-site-rgb-value), 0.65) 0%,
+    rgba(var(--background-site-rgb-value), 1) 100%
+  );
+}
+.embla:after {
+  bottom: -0.5px;
+  border-top: 0.5px solid rgba(var(--text-high-contrast-rgb-value), 0.3);
+  background: linear-gradient(
+    to bottom,
+    rgba(var(--background-site-rgb-value), 0.65) 0%,
+    rgba(var(--background-site-rgb-value), 1) 100%
+  );
+}
+.embla-picker {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  min-width: 100%;
+  justify-content: center;
+  line-height: 1;
+  font-size: 1.8rem;
+}
+.embla-picker__scene {
+  min-width: 100%;
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  touch-action: pan-x;
 }
 </style>
--- src/components/Picker.vue
+++ src/components/Picker.vue
@@ -1,6 +1,22 @@
+<template>
+  <div ref="emblaRef" class="embla-picker__viewport">
+    <div class="embla-picker__container">
+      <div
+        v-for="index in [...Array(60).keys()]"
+        :key="index"
+        class="embla-picker__slide"
+      >
+        {{ index }}
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import emblaCarouselVue from 'embla-carousel-vue';
+import { ref, onMounted } from "vue";
+import emblaCarouselVue from "embla-carousel-vue";
+import { WheelGesturesPlugin } from "embla-carousel-wheel-gestures";
+import type { EmblaCarouselType, EmblaOptionsType } from "embla-carousel";
 
 const CIRCLE_DEGREES = 360;
 const WHEEL_ITEM_SIZE = 30;
@@ -10,75 +26,29 @@ const WHEEL_ITEMS_IN_VIEW = 4;
 const WHEEL_ITEM_RADIUS = CIRCLE_DEGREES / WHEEL_ITEM_COUNT;
 const IN_VIEW_DEGREES = WHEEL_ITEM_RADIUS * WHEEL_ITEMS_IN_VIEW;
 const WHEEL_RADIUS = Math.round(
-  WHEEL_ITEM_SIZE / 2 / Math.tan(Math.PI / WHEEL_ITEM_COUNT)
+  WHEEL_ITEM_SIZE / 2 / Math.tan(Math.PI / WHEEL_ITEM_COUNT),
 );
 
-const [emblaRef, emblaApi] = emblaCarouselVue({
+const options = ref<EmblaOptionsType>({
   dragFree: true,
   containScroll: false,
   loop: false,
-  axis: 'y',
+  axis: "y",
   watchSlides: false,
 });
+const [emblaRef, emblaApi] = emblaCarouselVue(options, [WheelGesturesPlugin()]);
 
-onMounted(() => {
-  const slideCount = emblaApi.value.slideNodes().length;
-  const {
-    options: { loop },
-  } = emblaApi.value.internalEngine();
-  const totalRadius = slideCount * WHEEL_ITEM_RADIUS;
-  const rotationOffset = loop ? 0 : WHEEL_ITEM_RADIUS;
-  const rotateWheelFunc = rotateWheel(
-    emblaApi.value,
-    slideCount,
-    rotationOffset
-  );
-  const rotateSlidesFunc = rotateSlides(
-    emblaApi.value,
-    loop,
-    slideCount,
-    totalRadius
-  );
-  const rotate = (): void => {
-    rotateWheelFunc();
-    rotateSlidesFunc();
-  };
-
-  emblaApi.value.on('pointerUp', () => {
-    const { scrollTo, target, location } = emblaApi.value.internalEngine();
-    const diffToTarget = target.get() - location.get();
-    const factor = Math.abs(diffToTarget) < WHEEL_ITEM_SIZE / 2.5 ? 10 : 0.1;
-    const distance = diffToTarget * factor;
-    scrollTo.distance(distance, true);
-  });
-
-  emblaApi.value.on('scroll', rotate);
-
-  emblaApi.value.on('reInit', () => {
-    inactivateEmblaTransform(emblaApi.value);
-    rotate();
-  });
-
-  inactivateEmblaTransform(emblaApi.value);
-  rotate();
-});
-
-const slideIsInView = (
-  wheelLocation: number,
-  slidePosition: number
-): boolean => {
-  return Math.abs(wheelLocation - slidePosition) < IN_VIEW_DEGREES;
-};
+const slideIsInView = (wheelLocation: number, slidePosition: number): boolean =>
+  Math.abs(wheelLocation - slidePosition) < IN_VIEW_DEGREES;
 
 const rotateSlide = (
   emblaApi: EmblaCarouselType,
   index: number,
   loop: boolean,
   slideCount: number,
-  totalRadius: number
+  totalRadius: number,
 ): void => {
   const slideNode = emblaApi.slideNodes()[index];
-  console.log(emblaApi);
   const wheelLocation = emblaApi.scrollProgress() * totalRadius;
   const positionDefault = emblaApi.scrollSnapList()[index] * totalRadius;
   const positionLoopStart = positionDefault + totalRadius;
@@ -102,13 +72,13 @@ const rotateSlide = (
   }
 
   if (inView) {
-    slideNode.style.opacity = '1';
+    slideNode.style.opacity = "1";
     slideNode.style.transform = `translateY(-${
       index * 100
     }%) rotateX(${angle}deg) translateZ(${WHEEL_RADIUS}px)`;
   } else {
-    slideNode.style.opacity = '0';
-    slideNode.style.transform = 'none';
+    slideNode.style.opacity = "0";
+    slideNode.style.transform = "none";
   }
 };
 
@@ -116,7 +86,7 @@ const rotateSlides = (
   emblaApi: EmblaCarouselType,
   loop: boolean,
   slideCount: number,
-  totalRadius: number
+  totalRadius: number,
 ): (() => void) => {
   return (): void => {
     for (let index = 0; index < slideCount; index += 1) {
@@ -128,11 +98,10 @@ const rotateSlides = (
 const rotateWheel = (
   emblaApi: EmblaCarouselType,
   slideCount: number,
-  rotationOffset: number
+  rotationOffset: number,
 ): (() => void) => {
   return (): void => {
     const rotation = slideCount * WHEEL_ITEM_RADIUS - rotationOffset;
-    console.log(emblaApi);
     const wheelRotation = rotation * emblaApi.scrollProgress();
     emblaApi.containerNode().style.transform = `translateZ(${WHEEL_RADIUS}px) rotateX(${wheelRotation}deg)`;
   };
@@ -147,82 +116,82 @@ const inactivateEmblaTransform = (emblaApi: EmblaCarouselType): void => {
     translate.toggleActive(false);
   });
 };
+
+onMounted(() => {
+  if (!emblaApi.value) return;
+
+  const slideCount = emblaApi.value.slideNodes().length;
+  const {
+    options: { loop },
+  } = emblaApi.value.internalEngine();
+  const totalRadius = slideCount * WHEEL_ITEM_RADIUS;
+  const rotationOffset = loop ? 0 : WHEEL_ITEM_RADIUS;
+  const rotateWheelFunc = rotateWheel(
+    emblaApi.value,
+    slideCount,
+    rotationOffset,
+  );
+  const rotateSlidesFunc = rotateSlides(
+    emblaApi.value,
+    loop,
+    slideCount,
+    totalRadius,
+  );
+  const rotate = (): void => {
+    rotateWheelFunc();
+    rotateSlidesFunc();
+  };
+
+  emblaApi.value.on("pointerUp", () => {
+    if (!emblaApi.value) return;
+    const { scrollTo, target, location } = emblaApi.value.internalEngine();
+    const diffToTarget = target.get() - location.get();
+    const factor = Math.abs(diffToTarget) < WHEEL_ITEM_SIZE / 2.5 ? 10 : 0.1;
+    const distance = diffToTarget * factor;
+    scrollTo.distance(distance, true);
+  });
+
+  emblaApi.value.on("scroll", rotate);
+
+  emblaApi.value.on("reInit", () => {
+    if (!emblaApi.value) return;
+    inactivateEmblaTransform(emblaApi.value);
+    rotate();
+  });
+
+  inactivateEmblaTransform(emblaApi.value);
+  rotate();
+});
 </script>
 
-<template>
-  <div ref="emblaRef" class="embla__ios-picker__container">
-    <div class="embla__ios-picker__slide">0</div>
-    <div class="embla__ios-picker__slide">1</div>
-    <div class="embla__ios-picker__slide">2</div>
-    <div class="embla__ios-picker__slide">3</div>
-    <div class="embla__ios-picker__slide">4</div>
-    <div class="embla__ios-picker__slide">5</div>
-    <div class="embla__ios-picker__slide">6</div>
-    <div class="embla__ios-picker__slide">7</div>
-    <div class="embla__ios-picker__slide">8</div>
-    <div class="embla__ios-picker__slide">9</div>
-    <div class="embla__ios-picker__slide">10</div>
-    <div class="embla__ios-picker__slide">11</div>
-    <div class="embla__ios-picker__slide">12</div>
-    <div class="embla__ios-picker__slide">13</div>
-    <div class="embla__ios-picker__slide">14</div>
-    <div class="embla__ios-picker__slide">15</div>
-    <div class="embla__ios-picker__slide">16</div>
-    <div class="embla__ios-picker__slide">17</div>
-    <div class="embla__ios-picker__slide">18</div>
-    <div class="embla__ios-picker__slide">19</div>
-    <div class="embla__ios-picker__slide">20</div>
-    <div class="embla__ios-picker__slide">21</div>
-    <div class="embla__ios-picker__slide">22</div>
-    <div class="embla__ios-picker__slide">23</div>
-    <div class="embla__ios-picker__slide">24</div>
-    <div class="embla__ios-picker__slide">25</div>
-    <div class="embla__ios-picker__slide">26</div>
-    <div class="embla__ios-picker__slide">27</div>
-    <div class="embla__ios-picker__slide">28</div>
-    <div class="embla__ios-picker__slide">29</div>
-    <div class="embla__ios-picker__slide">30</div>
-    <div class="embla__ios-picker__slide">31</div>
-    <div class="embla__ios-picker__slide">32</div>
-    <div class="embla__ios-picker__slide">33</div>
-    <div class="embla__ios-picker__slide">34</div>
-    <div class="embla__ios-picker__slide">35</div>
-    <div class="embla__ios-picker__slide">36</div>
-    <div class="embla__ios-picker__slide">37</div>
-    <div class="embla__ios-picker__slide">38</div>
-    <div class="embla__ios-picker__slide">39</div>
-    <div class="embla__ios-picker__slide">40</div>
-    <div class="embla__ios-picker__slide">41</div>
-    <div class="embla__ios-picker__slide">42</div>
-    <div class="embla__ios-picker__slide">43</div>
-    <div class="embla__ios-picker__slide">44</div>
-    <div class="embla__ios-picker__slide">45</div>
-    <div class="embla__ios-picker__slide">46</div>
-    <div class="embla__ios-picker__slide">47</div>
-    <div class="embla__ios-picker__slide">48</div>
-    <div class="embla__ios-picker__slide">49</div>
-    <div class="embla__ios-picker__slide">50</div>
-    <div class="embla__ios-picker__slide">51</div>
-    <div class="embla__ios-picker__slide">52</div>
-    <div class="embla__ios-picker__slide">53</div>
-    <div class="embla__ios-picker__slide">54</div>
-    <div class="embla__ios-picker__slide">55</div>
-    <div class="embla__ios-picker__slide">56</div>
-    <div class="embla__ios-picker__slide">57</div>
-    <div class="embla__ios-picker__slide">58</div>
-    <div class="embla__ios-picker__slide">59</div>
-  </div>
-</template>
-
 <style scoped>
-.embla {
-  overflow: hidden;
+.embla-picker__viewport {
+  height: 32px;
+  width: 100%;
+  perspective: 1000px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-touch-callout: none;
+  -khtml-user-select: none;
+  -webkit-tap-highlight-color: transparent;
 }
-.embla__container {
+.embla-picker__container {
+  height: 100%;
+  width: 100%;
+  transform-style: preserve-3d;
+  will-change: transform;
+}
+.embla-picker__slide {
+  width: 100%;
+  height: 100%;
+  font-size: 19px;
+  text-align: center;
   display: flex;
-}
-.embla__slide {
-  flex: 0 0 100%;
-  min-width: 0;
+  align-items: center;
+  justify-content: center;
+  backface-visibility: hidden;
+  opacity: 1;
 }
 </style>
